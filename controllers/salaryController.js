7059bd6a72599e4b2d17bc0c38e44b1d00542703
Victor Palomo <victor.palomo@spore.cloud>
2021-09-10 00:33:37 -0500

    Implementa solucion para calcular el sueldo completo de los jugadores de resuelve @vickpalomo

--- controllers/salaryController.js
+++ controllers/salaryController.js
@@ -1,5 +1,11 @@
+const { teamDataToReplaceLevels, getPercentageTeam, getTotalSalary } = require('../lib/salary')
+const levels = require('../data/levels')
+
 const calculateSalary = (req, res) => {
-  return res.status(200).send({ code: 200, data: req.body, msg: 'Ok' })
+  const teamDataToGetSalary = teamDataToReplaceLevels(req.body.jugadores, levels)
+  const percentageTeam = getPercentageTeam(teamDataToGetSalary)
+  const teamDataFullSalary = getTotalSalary(teamDataToGetSalary, percentageTeam)
+  return res.status(200).send({ code: 200, data: teamDataFullSalary, msg: 'Ok' })
 }
 
 module.exports = {
